fbf9f4d35bd4f35de6272902d828dfd87fa5797f
Oleh_Pedorenko <Oleh_Pedorenko@EPUAKYIW2814>
2019-05-29 10:49:47 +0300

    task 08

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-import { SharedModule } from './../shared/shared.module';
-import { UsersServicesModule } from './users-services.module';
+import { SharedModule } from "./../shared/shared.module";
+import { UsersServicesModule } from "./users-services.module";
+import { UsersAPIProvider } from "./users.config";
 
 import {
   UsersRoutingModule,
   usersRouterComponents
-} from './users-routing.module';
-import { UserComponent } from './components';
+} from "./users-routing.module";
+import { UserComponent } from "./components";
 
 @NgModule({
   imports: [
@@ -19,6 +20,7 @@ import { UserComponent } from './components';
     UsersRoutingModule,
     UsersServicesModule
   ],
-  declarations: [usersRouterComponents, UserComponent]
+  declarations: [usersRouterComponents, UserComponent],
+  providers: [UsersAPIProvider]
 })
 export class UsersModule {}
