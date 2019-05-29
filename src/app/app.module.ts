057a1cf5fa9566290e35973c70192e98b952f655
Oleh_Pedorenko <Oleh_Pedorenko@EPUAKYIW2814>
2019-05-29 11:51:08 +0300

    Task 13 Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // add this line if you don't have access to
 // index.html and you want to set base tag
@@ -15,6 +15,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MyInterceptor } from './core/interceptors/my.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,6 +33,11 @@ import { AppComponent } from './app.component';
     // add this line if you don't have access to
     // index.html and you want to set base tag
     // { provide: APP_BASE_HREF, useValue: '/' }
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MyInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
